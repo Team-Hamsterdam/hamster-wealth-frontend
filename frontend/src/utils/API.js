17f78821e7986579d8123412a787994267ac552e
Matthew Lau <matthew.lau78@gmail.com>
2021-04-11 04:08:38 +1000

    changed api url

--- frontend/src/utils/API.js
+++ frontend/src/utils/API.js
@@ -5,5 +5,5 @@
 //   }
 // }
 
-export const api = "http://localhost:4500";
-// export const api = "https://hamsterwealth.herokuapp.com";
+// export const api = "http://localhost:4500";
+export const api = "https://hamsterwealth.herokuapp.com";
